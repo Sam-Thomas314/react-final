39dbbe486040e28ceb051fe7d1e67bb337dd18ff
Sam-Thomas314 <Samuelajt@gmail.com>
2024-11-05 11:07:06 -0500

    Trying to fix the home button to not remove all items from the cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -250,6 +250,12 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
+
+};
+const handleHomeClick = (e) => {
+    e.preventDefault();
+    setShowAboutUs(false);
+    setShowCart(false);
 };
 
 const handleContinueShopping = (e) => {
@@ -270,9 +276,9 @@ const handleContinueShopping = (e) => {
     }));
     
   };
-  
-  const newCartItems = useSelector((state) => state.cart.items);
 
+
+  
   const calculateTotalQuantity = () => {
     return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0) :0;
   };
@@ -291,31 +297,24 @@ const handleContinueShopping = (e) => {
 
     return (
         <div>
-             <div className="navbar" style={styleObj}>
+        <div className="navbar">
             <div className="tag">
-               <div className="luxury">
-               <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
-                        <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
-                    </div>
+                <div className="luxury">
+                    <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                    <a href="/" onClick={() => handleHomeClick(e)}>
+                        <div className="tag_home_link">
+                            <h3>Paradise Nursery</h3>
+                            <i>Where Green Meets Serenity</i>
+                        </div>
                     </a>
                 </div>
-              
+
             </div>
-            <div style={styleObjUl}>
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="/cart" onClick={(e) => handleCartClick(e)}><h1 
-                className='cart'><svg xmlns="http://www.w3.org/2000/svg" 
-                viewBox="0 0 256 256" id="IconChangeColor" height="68" 
-                width="68"><rect width="156" height="156" 
-                fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12">
-                    </circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" 
-                    fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round"
-                     stroke-width="2" id="mainIconPathAttribute"></path></svg>
-                     <div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>            
-                </div>
+            <div className="ul">
+                <div> <a href="/" onClick={() => handleHomeClick(e)}>Home</a></div> 
+                <div> <a href="/plants" onClick={(e) => handlePlantsClick(e)}>Plants</a></div>
+                <div> <a href="/cart" onClick={(e) => handleCartClick(e)}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>
+            </div>
         </div>
         {!showCart? (
         <div className="product-grid">
@@ -343,7 +342,7 @@ const handleContinueShopping = (e) => {
         </div>
     ) :  (
         <CartItem onContinueShopping={handleContinueShopping}/>
-    )}
+        )}
         </div>
         );
 }
